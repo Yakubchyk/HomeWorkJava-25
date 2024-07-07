89645ddd30a38a2449462ed9d07bf305a409c69e
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-07-07 11:39:39 +0200

    calc v1.0

---
+++ src/main/java/com/homeWork25/servlet/CalculatorServlet.java
@@ -0,0 +1,44 @@
+package com.homeWork25.servlet;
+
+import com.homeWork25.servlet.model.Operation;
+import com.homeWork25.servlet.servise.OperationService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/calculator")
+public class CalculatorServlet extends HttpServlet {
+
+    private final OperationService operationService = new OperationService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        double num1 = Double.parseDouble(req.getParameter("num1"));
+        double num2 = Double.parseDouble(req.getParameter("num2"));
+        String type = req.getParameter("type");
+
+        Operation operation = new Operation(num1, num2, type);
+
+        Operation result = operationService.getResult(operation);
+
+        String formated = "Result = %s".formatted(result.getResult());
+
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("<h3>num1 = " + num1 + "</h3>");
+        writer.println("<h3>num2 = " + num2 + "</h3>");
+        writer.println("<h3>type = " + type + "</h3>");
+
+        writer.println("<h1> " + formated + "</h1");
+        writer.println("</body>");
+        writer.println("</head>");
+    }
+}
---
+++ src/main/java/com/homeWork25/servlet/servise/OperationService.java
@@ -0,0 +1,24 @@
+package com.homeWork25.servlet.servise;
+
+import com.homeWork25.servlet.model.Operation;
+
+public class OperationService {
+    public Operation getResult(Operation operation) {
+        switch (operation.getType()) {
+            case "sum":
+                operation.setResult(operation.getNum1() + operation.getNum2());
+                return operation;
+            case "sub":
+                operation.setResult(operation.getNum1() - operation.getNum2());
+                return operation;
+            case "mul":
+                operation.setResult(operation.getNum1() * operation.getNum2());
+                return operation;
+            case "div":
+                operation.setResult(operation.getNum1() / operation.getNum2());
+                return operation;
+        }
+        throw new IllegalArgumentException("Operation type not supported");
+    }
+
+}
