a30be4ccec284f9c143ddcb0a7f8e7dea2524d07
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-07-07 21:58:42 +0200

    Listener Conntecst Add v1.3

--- src/main/java/com/homeWork25/listener/OperationListener.java
+++ src/main/java/com/homeWork25/listener/OperationListener.java
@@ -1,7 +1,26 @@
 package com.homeWork25.listener;
 
+import com.homeWork25.model.Operation;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
-public class OperationListener {
+public class OperationListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("Context Initialized");
+        ServletContext servletContext = sce.getServletContext();
+
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("Context Destroyed");
+
+    }
 }
